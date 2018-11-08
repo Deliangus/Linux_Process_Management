dda0a6b2139e9d274254912ce6049914ded8d01a
Yuxiang Luo <Deliangus@Gmail.com>
2018-11-08 15:54:48 -0500

    commit

--- src/syscall.c
+++ src/syscall.c
@@ -1,4 +1,22 @@
-#include "syscall.h"
+/*
+  A kernel module to list process by their names. Print when loading the module.
+  In the terminal, compile this module with "make" and load it with "sudo insmod process.ko". 
+  Check the result of printk by “dmesg”
+  Unload it with "sudo rmmod process.ko".
+*/
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/sched.h> // task_struct definition
+#include <asm/unistd.h>
+#include <linux/list.h>
+#include <linux/init_task.h>
+
+#ifndef __KERNEL__
+#define __KERNEL__
+#endif
+
 
 // Initialization of module
 int __init init_MyKernelModule(void)
