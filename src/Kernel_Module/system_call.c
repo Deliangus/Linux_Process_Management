3368dff54b92c7975054f1e7b2724b391052969e
Deliangus <Deliangus@Gmail.com>
2018-11-30 22:26:52 -0500

    final check

--- src/Kernel_Module/system_call.c
+++ src/Kernel_Module/system_call.c
@@ -17,7 +17,7 @@ static struct process process_List[512];
 static unsigned long syscall_getProcess(const struct process* user_List)
 {
 	struct task_struct * p;
-	int counter = 
+	int counter = 1;
 	unsigned long status;
 
 	p = NULL;
